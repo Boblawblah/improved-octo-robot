7bf06a11abc807768b3047b7e29e6f5dac8a81c3
Boblawblah <66443120+Boblawblah@users.noreply.github.com>
2020-06-21 16:43:38 -0600

    Add files via upload

---
+++ SimpleBankingApplication/src/com/gmail/matquantz/Main.java
@@ -0,0 +1,13 @@
+package com.gmail.matquantz;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount("XYZ", "BA000001");
+        bankAccount.showMenu();
+
+    }
+}
